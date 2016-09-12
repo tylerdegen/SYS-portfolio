f814fbff2810f6d31a398e6020f8dfbdb8457d1a
Tyler Degen <degen.19@osu.edu>
2016-09-12 00:30:31 -0400

    Removed Chris's substr fix
    Only necessary in very specific case

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
 <!-- begin footer -->
 
   <footer>
-    <img src="/seeyourspeech/images/SYS_footer.png" alt="See Your Speech" width=                                             "119" height="71">
+    <img src="/seeyourspeech/images/SYS_footer.png" alt="See Your Speech" width="119" height="71">
     <p>
     Explore the science hidden in your voice</p>
     <div id="contactUs">
@@ -11,16 +11,6 @@
       <a href="mailto:seeyourspeech@osu.edu">seeyourspeech@osu.edu</a>
     </div>
   </footer>
-
-<?php
-// CW 8/29
-//$dirlength = strlen(__DIR__)+1;
-$dirlength = strlen(__DIR__)-7;
-$perf = substr(substr($_SERVER["SCRIPT_FILENAME"], $dirlength),0,16);
-if ($perf == "perform_task.php") {
-    require_once("perform_footer.php");
-}
-?>
 
 </body>
 </html>
